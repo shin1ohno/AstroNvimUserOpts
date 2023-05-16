e57b7ea575d174904ca14e631482b710e930d092
Shin'ichi Ohno <ohno@cookpad.com>
2023-05-16 15:23:28 +0900

    Octo and around via community

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,4 +7,6 @@ return {
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.haskell" },
+  { import = "astrocommunity.git.octo" },
+  { import = "astrocommunity.motion.nvim-surround" }
 }
